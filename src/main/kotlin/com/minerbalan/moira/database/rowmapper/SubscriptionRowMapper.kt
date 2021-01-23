72704f4d8b1653a74bd63da2b53a5a1025784136
minerbalan <kasai_spring@icloud.com>
2021-01-23 14:33:07 +0900

    RowMapper追加

---
+++ src/main/kotlin/com/minerbalan/moira/database/rowmapper/SubscriptionRowMapper.kt
@@ -0,0 +1,19 @@
+package com.minerbalan.moira.database.rowmapper
+
+import com.minerbalan.moira.domain.entity.Subscription
+import org.springframework.jdbc.core.RowMapper
+import java.sql.ResultSet
+
+class SubscriptionRowMapper : RowMapper<Subscription> {
+    override fun mapRow(rs: ResultSet, rowNum: Int): Subscription {
+        return Subscription(
+            id = rs.getLong("id"),
+            name = rs.getString("name"),
+            url = rs.getString("url"),
+            createdAt = rs.getTimestamp("created_at").toLocalDateTime(),
+            lastFetchedAt = rs.getTimestamp("last_fetched_at").toLocalDateTime(),
+            updatedAt = rs.getTimestamp("updated_at")?.toLocalDateTime(),
+            deletedAt = rs.getTimestamp("deleted_at")?.toLocalDateTime()
+        )
+    }
+}
