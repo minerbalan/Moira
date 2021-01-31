79018c3a16349accc2245b7c595d2622c5a7a130
minerbalan <kasai_spring@icloud.com>
2021-01-31 23:58:38 +0900

    Subscription追加repository実装

--- src/main/kotlin/com/minerbalan/moira/database/SubscriptionsRepositoryImpl.kt
+++ src/main/kotlin/com/minerbalan/moira/database/SubscriptionsRepositoryImpl.kt
@@ -9,6 +9,15 @@ import org.springframework.stereotype.Repository
 @Repository
 class SubscriptionsRepositoryImpl(private val jdbcTemplate: JdbcTemplate) : SubscriptionsRepository {
     /**
+     * 新しくsubscriptionを追加する
+     */
+    override fun insertSubscription(subscription: Subscription) {
+        jdbcTemplate.update(
+                "INSERT INTO subscriptions(name, url, created_at, last_fetched_at) VALUES (?,?,?,?)",
+                subscription.name, subscription.url, subscription.createdAt, subscription.lastFetchedAt)
+    }
+
+    /**
      * 購読リストを取得する.
      */
     override fun fetchSubscriptionList(): List<Subscription?> {
--- src/main/kotlin/com/minerbalan/moira/domain/repository/SubscriptionsRepository.kt
+++ src/main/kotlin/com/minerbalan/moira/domain/repository/SubscriptionsRepository.kt
@@ -3,5 +3,13 @@ package com.minerbalan.moira.domain.repository
 import com.minerbalan.moira.domain.entity.Subscription
 
 interface SubscriptionsRepository {
+    /**
+     * 新しくsubscriptionを追加する
+     */
+    fun insertSubscription(subscription: Subscription)
+
+    /**
+     * 購読リストを取得する.
+     */
     fun fetchSubscriptionList(): List<Subscription?>
 }
