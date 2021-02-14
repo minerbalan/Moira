47f72e17e8c27eccd8bcb774552b5683c2368734
minerbalan <kasai_spring@icloud.com>
2021-02-15 00:31:19 +0900

    CSRFトークンを削除しCORS設定を追加

--- src/main/kotlin/com/minerbalan/moira/javaconfig/SpringSecurityConfig.kt
+++ src/main/kotlin/com/minerbalan/moira/javaconfig/SpringSecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.minerbalan.moira.javaconfig
 
+import com.minerbalan.moira.config.WebConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -8,10 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
-class SpringSecurityConfig : WebSecurityConfigurerAdapter() {
+class SpringSecurityConfig(val webConfig: WebConfig) : WebSecurityConfigurerAdapter() {
     //静的ファイルに認証をかけない
     override fun configure(web: WebSecurity) {
         web.ignoring().antMatchers("/favicon.ico", "/css/**", "/js/**")
@@ -19,11 +23,23 @@ class SpringSecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests().anyRequest().permitAll()
-        http.csrf().ignoringAntMatchers("/api/**")
+        http.csrf().disable()
+        http.cors().configurationSource(createCorsConfigSource())
     }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    private fun createCorsConfigSource(): CorsConfigurationSource {
+        val corsConfig = CorsConfiguration()
+        corsConfig.addAllowedHeader(CorsConfiguration.ALL)
+        corsConfig.allowedOrigins = webConfig.allowOrigin
+
+        val source = UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig)
+
+        return source
     }
 }
