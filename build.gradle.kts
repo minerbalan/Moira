383d906f0447963aaf0b087ad7f7a2ba6477f719
minerbalan <kasai_spring@icloud.com>
2021-07-11 20:27:18 +0900

    テストライブラリの追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+val kotlinVersion = "1.5.10"
+val junitJupiterVersion = "5.7.2"
+val junitPlatformEngineVersion = "1.7.2"
 
 plugins {
+    // build.gradle.ktsは現段階でpluginsスコープ外の変数にアクセスできないのでplugins内部でも変数を再定義する
+    val kotlinVersion = "1.5.10"
     id("org.springframework.boot") version "2.3.0.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.spring") version "1.5.10"
-    kotlin("plugin.jpa") version "1.5.10"
-    kotlin("kapt") version "1.5.10"
+    kotlin("jvm").version(kotlinVersion)
+    kotlin("plugin.spring").version(kotlinVersion)
+    kotlin("plugin.jpa").version(kotlinVersion)
+    kotlin("kapt").version(kotlinVersion)
 }
 
 group = "com.minerbalan"
@@ -19,43 +23,52 @@ repositories {
 }
 
 dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+    compileOnly("org.springframework.boot:spring-boot-configuration-processor")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.31.1")
     implementation("org.jetbrains.exposed:exposed-java-time:0.31.1")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    kapt("org.springframework.boot:spring-boot-configuration-processor")
-    compileOnly("org.springframework.boot:spring-boot-configuration-processor")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.session:spring-session-core")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-    }
-    testImplementation("org.springframework.security:spring-security-test")
-
-    implementation("com.fasterxml.jackson.core:jackson-databind")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
     implementation("org.apache.httpcomponents:httpclient:4.5.11")
     implementation("com.rometools:rome:1.15.0")
     implementation("org.jsoup:jsoup:1.13.1")
     implementation("mysql:mysql-connector-java:8.0.18")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(module= "mockito-core")
+    }
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:${kotlinVersion}")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:${kotlinVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitJupiterVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${junitJupiterVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitJupiterVersion}")
+    testRuntimeOnly("org.junit.platform:junit-platform-engine:${junitPlatformEngineVersion}")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:${junitPlatformEngineVersion}")
+    testRuntimeOnly("org.junit.platform:junit-platform-commons:${junitPlatformEngineVersion}")
+
+    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+    testImplementation("org.mockito:mockito-core:3.11.2")
+
+    testImplementation("com.h2database:h2:1.4.200")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
-    }
 }
 
 tasks.named<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun"){
